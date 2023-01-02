0877eac45dc182cd59312ac894df0152058d4802
yacine <lentente1958@gmail.com>
2023-01-02 23:49:40 +0100

    responsive style progress component

--- src/components/progress/index.tsx
+++ src/components/progress/index.tsx
@@ -3,10 +3,10 @@ function Progress({fraction}:{fraction:number}){
     const perc = (100-fraction)*560/100; //565.48
     return <>
         <div className="area_prog center"> 
-            <svg>
-                <circle r="90" cx="100" cy="100" fill="transparent"    strokeDasharray="560" strokeDashoffset="0"></circle>
-                <circle r="90" cx="100" cy="95" strokeDasharray="565.48" strokeDashoffset={perc}></circle>
-                <text x="75" y="110" fill="white">{fraction+"%"}</text>
+            <svg className="center">
+                <circle r="90" cx="50%" cy="50%" fill="transparent"    strokeDasharray="560" strokeDashoffset="0"></circle>
+                <circle r="90" cx="50%" cy="49%" strokeDasharray="565.48" strokeDashoffset={perc}></circle>
+                <text x="45%" y="55%" fill="white">{fraction+"%"}</text>
             </svg>  
         </div>
     </>
